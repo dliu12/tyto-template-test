a79c4a43334a8f0b6bf24850014b9df4998ab04b
Diyi Liu <diyi@immersedgames.com>
2025-08-05 16:55:52 -0400

    +update package json -remove dist

--- dist/main.js
+++
@@ -1,8 +0,0 @@
-import { MyButton as t } from "./components/MyButton/index.js";
-import { MyCounter as f } from "./components/MyCounter/index.js";
-import { MyTitle as p } from "./components/MyTitle/index.js";
-export {
-  t as MyButton,
-  f as MyCounter,
-  p as MyTitle
-};
